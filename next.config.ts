35cf31d997a561c479ab30048ea44d0893717a65
Atonu <atonu.zahin@gmail.com>
2026-06-18 19:41:45 +0600

    deployment rework

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
     domains: [],
-    unoptimized: false,
+    unoptimized: true,
   },
   transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
+  trailingSlash: true,
 };
 
 export default nextConfig;
